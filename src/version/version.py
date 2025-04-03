078498527ba20ddfe6192c4ce886bf4027e6fa63
YaoXuanZhi <yaoxuanzhi@outlook.com>
2025-04-03 23:47:38 +0800

    更新版本号

--- src/version/version.py
+++ src/version/version.py
@@ -3,7 +3,7 @@
 YEAR = 2023
 AUTHOR = "YaoXuanZhi"
 APP_NAME = "ScreenPinKit"
-VERSION = "2.6"
-HELP_URL = "https://pyqt-fluent-widgets.readthedocs.io"
+VERSION = "3.0"
+HELP_URL = "https://www.youtube.com/playlist?list=PL3uuKTASzjRYNdl7wYlgUd7agQIA2_y5V"
 FEEDBACK_URL = "https://github.com/YaoXuanZhi/ScreenPinKit/issues"
 RELEASE_URL = "https://github.com/YaoXuanZhi/ScreenPinKit/releases/latest"
